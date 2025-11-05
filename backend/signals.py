5be31c34aa6b21fd5e1f2d9c4372e304991c861e
Dmitry Pack <dmitrypack0@gmail.com>
2025-11-05 14:16:26 +0300

    edit signals.py

--- backend/signals.py
+++ backend/signals.py
@@ -5,11 +5,11 @@ from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver, Signal
 from django_rest_passwordreset.signals import reset_password_token_created
+from rest_framework.authtoken.models import Token
 
 from backend.models import ConfirmEmailToken, User
 
 new_user_registered = Signal()
-
 new_order = Signal()
 
 
@@ -17,23 +17,11 @@ new_order = Signal()
 def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
     """
     Отправляем письмо с токеном для сброса пароля
-    When a token is created, an e-mail needs to be sent to the user
-    :param sender: View Class that sent the signal
-    :param instance: View Instance that sent the signal
-    :param reset_password_token: Token Model Object
-    :param kwargs:
-    :return:
     """
-    # send an e-mail to the user
-
     msg = EmailMultiAlternatives(
-        # title:
         f"Password Reset Token for {reset_password_token.user}",
-        # message:
         reset_password_token.key,
-        # from:
         settings.EMAIL_HOST_USER,
-        # to:
         [reset_password_token.user.email]
     )
     msg.send()
@@ -42,20 +30,15 @@ def password_reset_token_created(sender, instance, reset_password_token, **kwarg
 @receiver(post_save, sender=User)
 def new_user_registered_signal(sender: Type[User], instance: User, created: bool, **kwargs):
     """
-     отправляем письмо с подтрердждением почты
+    Отправляем письмо с подтверждением почты
     """
     if created and not instance.is_active:
-        # send an e-mail to the user
         token, _ = ConfirmEmailToken.objects.get_or_create(user_id=instance.pk)
 
         msg = EmailMultiAlternatives(
-            # title:
-            f"Password Reset Token for {instance.email}",
-            # message:
+            f"Confirmation Token for {instance.email}",
             token.key,
-            # from:
             settings.EMAIL_HOST_USER,
-            # to:
             [instance.email]
         )
         msg.send()
@@ -64,19 +47,23 @@ def new_user_registered_signal(sender: Type[User], instance: User, created: bool
 @receiver(new_order)
 def new_order_signal(user_id, **kwargs):
     """
-    отправяем письмо при изменении статуса заказа
+    Отправляем письмо при изменении статуса заказа
     """
-    # send an e-mail to the user
     user = User.objects.get(id=user_id)
 
     msg = EmailMultiAlternatives(
-        # title:
-        f"Обновление статуса заказа",
-        # message:
+        "Обновление статуса заказа",
         'Заказ сформирован',
-        # from:
         settings.EMAIL_HOST_USER,
-        # to:
         [user.email]
     )
     msg.send()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """
+    Автоматически создаём токен после регистрации пользователя
+    """
+    if created:
+        Token.objects.create(user=instance)
